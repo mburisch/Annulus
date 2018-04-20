6fb391c07e7ac88bf1d90d16e3fa7ad9a525e202
Michael Burisch <michael.burisch@gmail.com>
2018-04-20 18:34:35 +0200

    Renamed files

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 
 from setuptools import setup
 
-long_description = "Detection of annuli (donuts) in images for camera calibration."
+long_description = """
+Detection of annuli (donuts) in images and recovery of a grid for camera calibration.
+"""
 	
 setup(
     name = 'annulus',
@@ -16,13 +18,10 @@ setup(
     keywords = "camera calibration marker detection",
 
     classifiers=[
-        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
     ],
     
-    #packages = find_packages(where = "annulus"),
-    #packages = find_packages(include = ["annulus"]),
-	packages = ["annulus"],
-	python_requires='>=3',
+    packages = ["annulus"],
+    python_requires='>=3'
 )
